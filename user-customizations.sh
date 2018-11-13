270ed4012869c29de5c6ea2a689205e0cdb18dd9
beer <christoph.beer@gmx.de>
2018-11-13 09:55:38 +0100

    updatet user-customisations.sh - npm install jquery

--- user-customizations.sh
+++ user-customizations.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "vagrantbox ------ > install bootstrap via Bower:"
+echo "vagrantbox ------ > install bootstrap and jquery via npm:"
 
 cd /home/vagrant/projekte/tutorials/symfony4/basics/public
 npm install bootstrap
+npm install jquery
 
 echo "vagrantbox ------ > DONE"
